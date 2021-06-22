432ae24a51a63c114fcb4ba480a0aa56f26d44c3
Kinga Buczniewska <kingabuczniewska@gmail.com>
2021-06-22 15:31:53 +0200

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -30,6 +30,14 @@ function App() {
       return task;
     }));
   }
+
+  const setAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true,
+    })));
+  }
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -52,6 +60,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
       />
